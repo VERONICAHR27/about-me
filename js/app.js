a446e2ed7bcc323da2d77b87f46bff6eed49625f
VERONICAHR27 <hilarioreyesveronica6@gmail.com>
2024-06-12 01:01:35 -0500

    Agregando contenido

--- js/app.js
+++ js/app.js
@@ -13,9 +13,10 @@ const respuesta5 = "NO";
 let respuesta_correcta = 0;
 let respuesta_incorrecta = 0;
 
-const nombre = prompt ("Cual es tu nombre");
-alert("Bienbenido "  + nombre + " ,te invito a conocer mas de mi");
 
+const nombre = prompt ("Hola, ¿Cual es tu nombre?");
+alert("Bienvenid@ "  + nombre + " ,te invito a conocer mas de mi");
+alert("Te mostrare 5 preguntas y debes adivinar con SI o NO  ");
 const Pregunta1 = prompt("¿Me gusta los deportes?");
 if(respuesta1.toLowerCase() === Pregunta1.toLowerCase()){
     console.log("Respuesta correcta 👏  " + nombre) 
@@ -26,7 +27,6 @@ if(respuesta1.toLowerCase() === Pregunta1.toLowerCase()){
     alert("Respuesta incorrecta 😢 " + nombre);
     respuesta_incorrecta += 1;
   }
-
 
 const Pregunta2 = prompt("¿Participe en alguna competencia Internacional?");
 if(respuesta2.toLowerCase() === Pregunta2.toLowerCase()){
@@ -71,9 +71,61 @@ if(respuesta5.toLowerCase() === Pregunta5.toLowerCase()){
   alert("Respuesta incorrecta 😢 " + nombre);
   respuesta_incorrecta += 1;
 }
+alert("Continuemos, te hare una pregunta y tendras 4 intentos para poder adivinar, suerte");
+const numeroIntentos = 4 ;
+const numeroAdivinar= 23;
+let contadorintentos= 1; 
 
-  alert("Adivinaste " + respuesta_correcta + " preguntas " + nombre + ".")
-  alert("Oh, "+ nombre + " tienes "+ respuesta_incorrecta + " preguntas erroneas.")
+while(contadorintentos <=numeroIntentos){
+  const numeroUsuario = Number (prompt("¿Cual es mi edad?"));
+  if(numeroUsuario===numeroAdivinar){
+    alert("Adivinaste, tengo " + numeroAdivinar + " años. Lograste en el " + contadorintentos + " intento ")
+    respuesta_correcta += 1;
+    break;
+  }else{
+    contadorintentos++;
+    if(contadorintentos===numeroIntentos + 1){
+      alert("oh no! Llegaste al numero maximo de intentos");
+      respuesta_incorrecta += 1;
+    }else{
+      if (numeroUsuario < numeroAdivinar){
+        alert("Mi edad  es mayor que " + numeroUsuario);
+      }else{
+        alert("Mi edad es menor que " + numeroUsuario);
+      }
+    }
+  }
+}
+alert("Estamos por terminar, para la ultima pregunta tendras 6 intentos, suerte");
+
+let correcta=0;
+const paises=["ecuador", "argentina","africa","brazil","bolivia"];
+let respuestaUsuario=prompt("Menciona algunos paises que conoci con el deporte");
+
+
+for (let i=1;i<6;i++){
+  for(let j=0; j < 6; j++){
+    if(respuestaUsuario===paises[j]){
+      alert("Exelente, Uno de los paises que conoci es " + respuestaUsuario);
+      correcta=1;
+      respuesta_correcta += 1;
+      break;
+    }
+  }
+  if(correcta==1){
+    break;
+  }else {
+    respuestaUsuario = prompt("Sigue intentando");
+  }
+  if (i==5 && correcta==0){
+    alert("oh no! Llegaste al numero maximo de intentos");
+    alert(paises[i]);
+    respuesta_incorrecta += 1;
+  }
+}
+alert("A continuacion te mostrare cuantas preguntas acertaste o no");
+  alert("Adivinaste " + respuesta_correcta + " preguntas " + nombre + ".");
+  alert("Oh, "+ nombre + " tienes "+ respuesta_incorrecta + " preguntas erroneas.");
   
 
 
